c73b994edf1b827fd7508af9bfb6b097d67cd214
dthanos <dthanos95@gmail.com>
2024-11-21 12:42:29 +0200

    vuetify

---
+++ src/config/vuetify.ts
@@ -0,0 +1,26 @@
+import 'vuetify/styles'
+import {createVuetify} from 'vuetify'
+import * as components from 'vuetify/components'
+import * as labs from 'vuetify/labs/components'
+import * as directives from 'vuetify/directives'
+
+const myCustomLightTheme = {
+    dark: false,
+}
+createVuetify({
+    components: {
+        ...components,
+        ...labs
+    },
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+    },
+    theme: {
+        defaultTheme: 'myCustomLightTheme',
+        themes: {
+            myCustomLightTheme,
+        },
+    },
+})
+
