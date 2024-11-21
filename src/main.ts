dbca363da59cce10c8db06aae899a0e53ea31560
dthanos <dthanos95@gmail.com>
2024-11-21 13:12:09 +0200

    pinia + axios

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import './style.css'
 import App from './App.vue'
 import vuetify from './config/vuetify'
 import router from './config/router'
+import { createPinia } from 'pinia'
+
+const pinia = createPinia()
 
 createApp(App)
     .use(vuetify)
     .use(router)
+    .use(pinia)
     .mount('#app')
