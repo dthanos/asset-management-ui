b432313f675973e494fd267f718f6c0242d9323c
thanos <dthanos95@gmail.com>
2024-11-27 01:04:39 +0200

    duplicate api call fix

--- src/stores/asset.ts
+++ src/stores/asset.ts
@@ -20,19 +20,15 @@ export const useAssetStore = defineStore('asset', () => {
 
     fetchData();
 
-    function onFilterUpdated() {
-        datatableStore.api.index = `${import.meta.env.VITE_API_URL}/listings?filter[type_id]=${filters.value?.type?.value ?? filters.value?.type}&filter[amenities]=${filters.value?.amenities?.join(',') ?? ''}`;
-        datatableStore.fetchData()
-    }
     async function fetchData(){
         loading.value = true;
         amenities.value = await indexAmenities()
         types.value = await indexTypes()
         filters.value.amenities = route.query['filter[amenities]']?.split(',');
         filters.value.type = types?.value?.map((i: any, index: number) => {return {value: index + 1, title: i.name}}).find(i => i.value == route.query['filter[type_id]']);
+        datatableStore.api.index = `${import.meta.env.VITE_API_URL}/listings?filter[type_id]=${filters.value?.type?.value ?? filters.value?.type}&filter[amenities]=${filters.value?.amenities?.join(',') ?? ''}`;
         datatableStore.meta = { page: Number(route.query.page) || 1, itemsPerPage: Number(route.query.itemsPerPage) || 15 };
         loading.value = false;
-        onFilterUpdated();
     }
     async function fetchAsset(id: string){
         loading.value = true;
@@ -72,7 +68,6 @@ export const useAssetStore = defineStore('asset', () => {
         isDirty,
         form,
 
-        onFilterUpdated,
         fetchAsset,
         editAsset,
         createAsset,
